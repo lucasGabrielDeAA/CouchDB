c3a296f62752bed3edb2878fc84b07fbd07de5c9
lucasGabriel <lg.technus@gmail.com>
2014-12-09 10:26:45 -0200

    Classe Produto

---
+++ produto.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+
+import json
+
+##Classe Produto com seus respectivos atributos
+class Produto:
+	#Construtor default
+	def __init__(self):
+		Produto.__init__(self)
+
+	#Contrutor sobrecarregado
+	def __init__(self, _id, nome, descricao, preco, quantidade):
+		self._id = _id
+		self.nome = nome
+		self.descricao = descricao
+		self.preco = preco
+		self.quantidade = quantidade++
+
+	def toString(self):
+		return json.dumps({'id': self._id, 'nome':self.nome, 'descricao':self.descricao, 'preco':self.preco, 'quantidade':self.quantidade})
